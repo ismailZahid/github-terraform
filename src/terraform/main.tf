8c44ebce20e39afc0b3066543298df07e06f2468
T155747 <iszahid-ext@airfrance.fr>
2024-11-06 15:47:36 +0100

    Initial Terraform setup with resource group

---
+++ src/terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-resources"
+  location = "West Europe"
+}
